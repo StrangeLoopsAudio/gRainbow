3766e5168262396c0b5bd9a4e69b2b9a06838914
Nick Merfeld <nrmvlad27@gmail.com>
2021-08-04 11:32:35 -0500

    filter type buttons added

--- Source/FilterControl.cpp
+++ Source/FilterControl.cpp
@@ -33,6 +33,46 @@ void FilterControl::paint(juce::Graphics& g) {
   g.setFont(14.0f);
   g.drawText("FilterControl", getLocalBounds(), juce::Justification::centred,
              true);  // draw some placeholder text
+
+  // draw filter type rectangles
+  float filterTypeHeight = 40;
+  float filterTypeWidth = getWidth() / 3;
+  float curStart = 1.0f;
+  for (int i = 0; i < 3; i++) {
+    juce::Colour tabColour = juce::Colours::darkgrey;
+    /*float tabHeight =
+        (mCurSelectedTab == i) ? getHeight() + 20.0f : getHeight() - 2.0f;*/
+    juce::Rectangle<float> filterTypeRect =
+        juce::Rectangle<float>(curStart, 1.0f, filterTypeWidth, filterTypeHeight);
+   /* if (mCurHoverTab == i && mCurSelectedTab != i) {
+      g.setColour(tabColour.withAlpha(0.3f));
+      g.fillRoundedRectangle(tabRect, 10.0f);
+    }*/
+    g.setColour(tabColour);
+    g.drawRoundedRectangle(filterTypeRect, 10.0f, 2.0f);
+
+    g.setColour(juce::Colours::white);
+    switch (i) {
+      case 0:
+        g.drawText(juce::String(Utils::FilterType::LOWPASS),
+          filterTypeRect.withHeight(getHeight() - 2.0f),
+          juce::Justification::centred);
+        break;
+
+      case 1:
+        g.drawText(juce::String(Utils::FilterType::BANDPASS),
+          filterTypeRect.withHeight(getHeight() - 2.0f),
+          juce::Justification::centred);
+        break;
+
+      case 2:
+        g.drawText(juce::String(Utils::FilterType::HIGHPASS),
+          filterTypeRect.withHeight(getHeight() - 2.0f),
+          juce::Justification::centred);
+        break;
+    }
+    curStart += filterTypeWidth + 2.0f;
+  }
 }
 
 void FilterControl::resized() {
